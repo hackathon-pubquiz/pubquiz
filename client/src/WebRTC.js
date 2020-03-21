b4b2f5049da601f816d640e288582848b04e47fc
Maike Paetzel <maike.paetzel@it.uu.se>
2020-03-21 17:04:50 +0100

    Fixed some semicolons

--- client/src/WebRTC.js
+++ client/src/WebRTC.js
@@ -3,16 +3,15 @@ import Peer from "peerjs";
 
 const WebRTC = () => {
 
-    const [peer, setPeer] = useState(null)
-    const [myID, setMyID] = useState('')
-    const [partnerID, setPartnerID] = useState('')
-    const [locked, setLocked] = useState(false)
-    const [messageContent, setMessageContent] = useState(null)
-    const [messageHistory, setMessageHistory] = useState([])
-    const [partners, setPartners] = useState([])
+    const [peer, setPeer] = useState(null);
+    const [myID, setMyID] = useState('');
+    const [partnerID, setPartnerID] = useState('');
+    const [messageContent, setMessageContent] = useState(null);
+    const [messageHistory, setMessageHistory] = useState([]);
+    const [partners, setPartners] = useState([]);
 
     useEffect(() => {
-        const peer = new Peer()
+        const peer = new Peer();
         peer.on('open', function (id) {
             setMyID(id)
         });
@@ -33,7 +32,7 @@ const WebRTC = () => {
             });
         });
         setPeer(peer)
-    }, [])
+    }, []);
 
 
     const add_partner = (p) => {
@@ -45,10 +44,10 @@ const WebRTC = () => {
                     peerId: p,
                     connection: connection
                 }]
-        })
+        });
         setPartnerID('')
 
-    }
+    };
 
 
     const connect_send = (messageContent) => {
@@ -56,7 +55,7 @@ const WebRTC = () => {
         partners.forEach((p) => {
             p.connection.on('error', console.log);
             p.connection.send(messageContent);
-        })
+        });
         setMessageHistory((prevState) => {
             return [
                 ...prevState,
@@ -64,33 +63,33 @@ const WebRTC = () => {
                     sender: myID,
                     content: messageContent
                 }]
-        })
+        });
         setMessageContent('')
-    }
+    };
 
     const partnerContent = partners.map((e) => {
         return <div>{e.peerId}</div>
-    })
+    });
 
     const computeHistory = messageHistory.map((e) => {
         return <div>{e.sender}: {e.content}</div>
-    })
+    });
 
     return (
         <div>
             <div>My WebRTC ID is: {myID} </div>
             <div>Partner ID: {partnerContent}</div>
             <div>
-                <input disabled={locked} value={partnerID} onChange={(e) => setPartnerID(e.target.value)}/>
-                <button disabled={locked} onClick={() => add_partner(partnerID)}>Add a partner</button>
+                <input value={partnerID} onChange={(e) => setPartnerID(e.target.value)}/>
+                <button onClick={() => add_partner(partnerID)}>Add a partner</button>
             </div>
             <div>
-                <input disabled={locked} value={messageContent} onChange={(e) => setMessageContent(e.target.value)}/>
-                <button disabled={locked} onClick={() => connect_send(messageContent)}>Send Message</button>
+                <input value={messageContent} onChange={(e) => setMessageContent(e.target.value)}/>
+                <button onClick={() => connect_send(messageContent)}>Send Message</button>
             </div>
             <div>{computeHistory}</div>
         </div>
     );
-}
+};
 
 export default WebRTC;
