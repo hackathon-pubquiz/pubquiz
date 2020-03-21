e6f708c70b26b64e23374dfad79662e5f9d8448a
Manuel Dossinger <git@manueldossinger.de>
2020-03-21 20:01:51 +0100

    Nicer chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Link as RouterLink, Route, Switch } from "react-router-dom";
 import io from "socket.io-client";
-import ChatWrapper from "./ChatWrapper";
+import clsx from "clsx";
+
+import ChatWrapper from "./TextChat/ChatWrapper";
 import Groups from "./Groups";
 import Persons from "./Persons";
 import QuizMaster from "./pages/QuizMaster";
@@ -18,14 +20,20 @@ import TeamChooser from "./TeamChooser";
 import { Tabs, Tab, Grid, AppBar, Typography } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { connect } from "react-redux";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import ChatIcon from "@material-ui/icons/Chat";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 const socket = io({
   autoConnect: false
 });
+
+const drawerWidth = 240;
 
 function LoginStub(props) {
   const [nickname, setNickname] = useState("");
@@ -52,6 +60,44 @@ const styles = theme => ({
     "& > :last-child": {
       flexGrow: 1
     }
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap"
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
   }
 });
 
@@ -60,7 +106,23 @@ class App extends React.Component {
     socket.open();
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false };
+  }
+
   render() {
+    const handleDrawerOpen = () => {
+      console.log("Opening drawer");
+      this.setState({ open: true });
+    };
+
+    const handleDrawerClose = () => {
+      console.log("Closing drawer");
+      this.setState({ open: false });
+    };
+
     const profileElement = nickname => (
       <div>
         <Button
@@ -78,33 +140,59 @@ class App extends React.Component {
     );
 
     const { classes } = this.props;
+
+    const open = this.state.open;
     return (
       <Router>
         <CssBaseline />
 
+        <AppBar
+          position="static"
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open
+          })}
+        >
+          <Tabs component="nav" value={false}>
+            <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start">
+              <ChatIcon />
+            </IconButton>
+            <Tab component={RouterLink} to="/pubs" label="Pubs"></Tab>
+            <Tab component={RouterLink} to="/groups" label="Gruppen"></Tab>
+            <Tab component={RouterLink} to="/people" label="Personen"></Tab>
+            <Tab component={RouterLink} to="/player" label="Player"></Tab>
+            <Tab component={RouterLink} to="/quizmaster" label="Quizmaster"></Tab>
+            <Tab component={RouterLink} to="/aktuellesQuiz" label="Aktuelles Quiz"></Tab>
+            {this.props.authenticated ? (
+              profileElement(this.props.loggedInUser.nickname)
+            ) : (
+              <Tab component={RouterLink} to="/login" label="Login"></Tab>
+            )}
+            <Tab component={RouterLink} to="/login2" label="Login"></Tab>
+          </Tabs>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open
+            })
+          }}
+        >
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+
+          <ChatWrapper socket={socket} open={open} />
+        </Drawer>
+
         <Grid container direction="column" className={classes.main}>
-          <Grid item>
-            <AppBar position="static">
-              <Tabs component="nav" value={false}>
-                <Tab component={RouterLink} to="/pubs" label="Pubs"></Tab>
-                <Tab component={RouterLink} to="/groups" label="Gruppen"></Tab>
-                <Tab component={RouterLink} to="/people" label="Personen"></Tab>
-                <Tab component={RouterLink} to="/player" label="Player"></Tab>
-                <Tab component={RouterLink} to="/quizmaster" label="Quizmaster"></Tab>
-                <Tab component={RouterLink} to="/aktuellesQuiz" label="Aktuelles Quiz"></Tab>
-                {this.props.authenticated ? (
-                  profileElement(this.props.loggedInUser.nickname)
-                ) : (
-                  <Tab component={RouterLink} to="/login" label="Login"></Tab>
-                )}
-                <Tab component={RouterLink} to="/login2" label="Login"></Tab>
-              </Tabs>
-            </AppBar>
-          </Grid>
           <Grid item container justify="space-evenly" alignItems="stretch">
-            <Grid item>
-              <ChatWrapper socket={socket} />
-            </Grid>
             <Grid item>
               <Switch>
                 <Route path="/login">
